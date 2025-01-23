d12781f6a96987618ed92892a44b3bedf3df5afb
WillKirkmanM <WillKirkmanMoller@gmail.com>
2025-01-23 23:34:09 +0000

    Feat(main.rs): Entrypoint to Executing X Files

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use clap::Parser;
+use codegen::CodeGen;
+use inkwell::context::Context;
+use parser::parse_file;
+use std::error::Error;
+use std::fs;
+pub mod ast;
+pub mod codegen;
+pub mod parser;
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    file: String,
+    #[arg(long)]
+    debug: bool,
+    #[arg(long)]
+    emit_llvm: bool,
+}
+fn main() -> Result<(), Box<dyn Error>> {
+    let args = Args::parse();
+    let content = fs::read_to_string(&args.file)?;
+    let exprs = parse_file(&content);
+    let context = Context::create();
+    let mut codegen = CodeGen::new(&context)?;
+    if args.emit_llvm {
+        println!("LLVM IR:");
+        codegen.module.print_to_stderr();
+    }
+    let main_fn = codegen
+        .generate_code(&exprs, args.debug)
+        .ok_or("Failed to compile")?;
+    unsafe {
+        main_fn.call();
+    }
+    Ok(())
+}
+
