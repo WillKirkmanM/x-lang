2758806ed9528cc044219f1d21654b033997727b
WillKirkmanM <WillKirkmanMoller@gmail.com>
2025-01-27 00:36:08 +0000

    Feat(bitand.rs): Implementation of Binary Operation bitand

---
+++ src/binary_ops/bitand.rs
@@ -0,0 +1,17 @@
+use inkwell::values::{IntValue, FunctionValue};
+use crate::ast::Expr;
+use crate::codegen::CodeGen;
+
+pub fn build_bitand<'ctx>(
+    codegen: &mut CodeGen<'ctx>,
+    left: &Box<Expr>, 
+    right: &Box<Expr>,
+    function: FunctionValue<'ctx>,
+    printf: FunctionValue<'ctx>, 
+    debug: bool,
+    in_print: bool,
+) -> Option<IntValue<'ctx>> {
+    let lhs = codegen.generate_expr(left, function, printf, debug, in_print)?;
+    let rhs = codegen.generate_expr(right, function, printf, debug, in_print)?;
+    Some(codegen.builder.build_and(lhs, rhs, "bitand").unwrap())
+}
