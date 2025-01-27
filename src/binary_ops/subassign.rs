18a9c95b22e9e1b2af590dfd065e348ef0d593b8
WillKirkmanM <WillKirkmanMoller@gmail.com>
2025-01-27 00:36:19 +0000

    Feat(subassign.rs): Implementation of Binary Operation subassign

---
+++ src/binary_ops/subassign.rs
@@ -0,0 +1,23 @@
+use inkwell::values::{IntValue, FunctionValue};
+use crate::ast::Expr;
+use crate::codegen::CodeGen;
+
+pub fn build_subassign<'ctx>(
+    codegen: &mut CodeGen<'ctx>,
+    left: &Box<Expr>, 
+    right: &Box<Expr>,
+    function: FunctionValue<'ctx>,
+    printf: FunctionValue<'ctx>, 
+    debug: bool,
+    in_print: bool,
+) -> Option<IntValue<'ctx>> {
+    let lhs = codegen.generate_expr(left, function, printf, debug, in_print)?;
+    let rhs = codegen.generate_expr(right, function, printf, debug, in_print)?;
+    let new_val = codegen.builder.build_int_sub(lhs, rhs, "subassign").unwrap();
+    if let Expr::String(name) = &**left {
+    if let Some(&ptr) = codegen.variables.get(name) {
+    codegen.builder.build_store(ptr, new_val).unwrap();
+    }
+    }
+    Some(new_val)
+}
