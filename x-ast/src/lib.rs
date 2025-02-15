22e2c1f5cca7749d00d5f3fc8405abf46654d307
WillKirkmanM <WillKirkmanMoller@gmail.com>
2025-02-15 12:47:54 +0000

    Feat(x-ast/lib): Add Function Calls, String & Identifier Operators & Import Statements

--- x-ast/src/lib.rs
+++ x-ast/src/lib.rs
@@ -9,20 +9,24 @@ pub enum Operator {
 #[derive(Debug, Clone, PartialEq)]
 pub enum Expr {
     Number(i64),
-    Add(Box<Expr>, Box<Expr>),
-    Multiply(Box<Expr>, Box<Expr>),
+    String(String),
+    Identifier(String),
     BinaryOp {
         left: Box<Expr>,
         op: Operator,
-        right: Box<Expr>,
+        right: Box<Expr>
     },
-    Identifier(String),
+    FunctionCall {
+        name: String,
+        args: Vec<Expr>
+    },
 }
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum Statement {
     Expression { expr: Expr },
     VariableDecl { name: String, value: Expr },
+    Import { module: String, item: String },
 }
 
 #[derive(Debug, Clone, PartialEq)]
