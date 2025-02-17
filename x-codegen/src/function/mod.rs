efa70e07f3b784e1133157e00b19731b4f66d914
WillKirkmanM <WillKirkmanMoller@gmail.com>
2025-02-17 15:23:50 +0000

    Feat(x-codegen/function): Handle Variable Scopes in Functions

--- x-codegen/src/function/mod.rs
+++ x-codegen/src/function/mod.rs
@@ -1,6 +1,5 @@
 use inkwell::values::{FloatValue, FunctionValue};
 use x_ast::{Expr, Statement};
-use inkwell::values::BasicValueEnum;
 
 use crate::CodeGen;
 
@@ -54,49 +53,42 @@ impl<'ctx> CodeGen<'ctx> {
     pub fn compile_function(
         &mut self,
         name: &str,
-        args: &[String],
-        body: &Statement,
+        params: &[String],
+        body: &[Statement],
     ) -> Result<FunctionValue<'ctx>, String> {
         let f64_type = self.context.f64_type();
-        let arg_types = vec![f64_type.into(); args.len()];
-        let fn_type = f64_type.fn_type(&arg_types, false);
-    
+        let fn_type = f64_type.fn_type(&vec![f64_type.into(); params.len()], false);
         let function = self.module.add_function(name, fn_type, None);
-        let basic_block = self.context.append_basic_block(function, "entry");
-        self.builder.position_at_end(basic_block);
-    
+        
+        let entry = self.context.append_basic_block(function, "entry");
+        self.builder.position_at_end(entry);
+        
         let old_vars = self.variables.clone();
-        for (i, arg) in args.iter().enumerate() {
-            let arg_value = function.get_nth_param(i as u32).unwrap().into_float_value();
-            let arg_alloca = self.create_entry_block_alloca(function, arg);
-            self.builder.build_store(arg_alloca, arg_value).unwrap();
-            self.variables.insert(arg.clone(), arg_value);
-        }
-    
-        // Handle both expressions and blocks
-        let return_value = match body {
-            Statement::Expression { expr } => self.gen_expr(expr)?,
-            Statement::Block { statements } => {
-                let mut last_value = self.context.f64_type().const_float(0.0);
-                for stmt in statements {
-                    if let Some(val) = self.gen_statement(stmt)? {
-                        last_value = val;
-                    }
-                }
-                last_value
+        
+        for (i, param) in params.iter().enumerate() {
+            let alloca = self.builder.build_alloca(f64_type, param)
+                .map_err(|e| e.to_string())?;
+            
+            if let Some(param_value) = function.get_nth_param(i as u32) {
+                self.builder.build_store(alloca, param_value)
+                    .map_err(|e| e.to_string())?;
             }
-            _ => return Err("Function body must be an expression or block".to_string()),
-        };
-    
-        self.builder.build_return(Some(&return_value)).unwrap();
-        self.variables = old_vars;
-    
-        if function.verify(true) {
-            self.register_function(name.to_string(), function);
-            Ok(function)
-        } else {
-            Err("Invalid generated function.".to_string())
+            
+            self.variables.insert(param.clone(), alloca);
         }
+        
+        let mut last_value = self.context.f64_type().const_float(0.0);
+        for stmt in body {
+            if let Some(val) = self.gen_statement(stmt)? {
+                last_value = val;
+            }
+        }
+        
+        self.builder.build_return(Some(&last_value))
+            .map_err(|e| e.to_string())?;
+        
+        self.variables = old_vars;
+        
+        Ok(function)
     }
-
 }
