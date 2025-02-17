c36ef69004140e3c0a77d32fcdd8a0b39c56c43f
WillKirkmanM <WillKirkmanMoller@gmail.com>
2025-02-17 16:22:10 +0000

    Feat(x-codegen/expression): Add Comparison Operators

--- x-codegen/src/expression/mod.rs
+++ x-codegen/src/expression/mod.rs
@@ -1,5 +1,5 @@
 use crate::CodeGen;
-use inkwell::values::{FloatValue, PointerValue};
+use inkwell::{values::{FloatValue, PointerValue}, FloatPredicate};
 use x_ast::{Expr, Operator};
 
 impl<'ctx> CodeGen<'ctx> {
@@ -31,6 +31,12 @@ impl<'ctx> CodeGen<'ctx> {
                         .map_err(|e| e.to_string())?),
                     Operator::Divide => Ok(self.builder.build_float_div(lhs, rhs, "divtmp")
                         .map_err(|e| e.to_string())?),
+                    Operator::LessThan => self.gen_comparison(FloatPredicate::OLT, lhs, rhs),
+                    Operator::GreaterThan => self.gen_comparison(FloatPredicate::OGT, lhs, rhs),
+                    Operator::LessThanOrEqual => self.gen_comparison(FloatPredicate::OLE, lhs, rhs),
+                    Operator::GreaterThanOrEqual => self.gen_comparison(FloatPredicate::OGE, lhs, rhs),
+                    Operator::Equal => self.gen_comparison(FloatPredicate::OEQ, lhs, rhs),
+                    Operator::NotEqual => self.gen_comparison(FloatPredicate::ONE, lhs, rhs),
                 }
             },
             Expr::FunctionCall { name, args } => self.gen_function_call(name, args),
@@ -38,6 +44,25 @@ impl<'ctx> CodeGen<'ctx> {
                 Err("String literals can only be used in print function calls".to_string())
             },
         }
+    }
+
+    fn gen_comparison(
+        &self,
+        predicate: FloatPredicate,
+        lhs: FloatValue<'ctx>,
+        rhs: FloatValue<'ctx>
+    ) -> Result<FloatValue<'ctx>, String> {
+        let cmp = self.builder
+            .build_float_compare(predicate, lhs, rhs, "cmptmp")
+            .map_err(|e| e.to_string())?;
+        
+        Ok(self.builder
+            .build_unsigned_int_to_float(
+                cmp,
+                self.context.f64_type(),
+                "booltmp"
+            )
+            .map_err(|e| e.to_string())?)
     }
 
     pub fn gen_print_str(&self, args: &[Expr]) -> Result<FloatValue<'ctx>, String> {
