6d8bd72f19f3d2eb28bdd2e269cc376ef904bebc
WillKirkmanM <WillKirkmanMoller@gmail.com>
2025-02-17 13:36:24 +0000

    Feat(x-codegen/expression): Move gen_function_call into Function Module & Make gen_print_str Public

--- x-codegen/src/expression/mod.rs
+++ x-codegen/src/expression/mod.rs
@@ -31,44 +31,7 @@ impl<'ctx> CodeGen<'ctx> {
         }
     }
 
-    fn gen_function_call(&self, name: &str, args: &[Expr]) -> Result<FloatValue<'ctx>, String> {
-        match (name, args.first()) {
-            ("print", Some(Expr::String(_))) => self.gen_print_str(args),
-            ("print", _) => self.gen_print(args),
-            _ => {
-                let f = if let Some(f) = self.functions.get(name) {
-                    *f
-                } else if let Some(f) = self.imported_functions.get(name) {
-                    *f
-                } else if let Some(f) = self.module.get_function(name) {
-                    f
-                } else {
-                    return Err(format!("Unknown function {}", name));
-                };
-    
-                let compiled_args: Vec<_> = args.iter()
-                    .map(|arg| self.gen_expr(arg))
-                    .collect::<Result<Vec<_>, _>>()?
-                    .into_iter()
-                    .map(|val| val.into())
-                    .collect();
-    
-                let argslen = f.count_params() as usize;
-                if argslen != args.len() {
-                    return Err(format!("Expected {} arguments, got {}", argslen, args.len()));
-                }
-    
-                match self.builder.build_call(f, &compiled_args, "calltmp")
-                    .map_err(|e| e.to_string())?
-                    .try_as_basic_value()
-                    .left() {
-                    Some(value) => Ok(value.into_float_value()),
-                    None => Err("Invalid call produced void value".to_string())
-                }
-            }
-        }
-    }
-    fn gen_print_str(&self, args: &[Expr]) -> Result<FloatValue<'ctx>, String> {
+    pub fn gen_print_str(&self, args: &[Expr]) -> Result<FloatValue<'ctx>, String> {
         if let Some(print_str_fn) = self.imported_functions.get("print_str") {
             let string_ptr = if let Expr::String(s) = &args[0] {
                 self.builder
@@ -91,7 +54,7 @@ impl<'ctx> CodeGen<'ctx> {
         }
     }
 
-    fn gen_print(&self, args: &[Expr]) -> Result<FloatValue<'ctx>, String> {
+    pub fn gen_print(&self, args: &[Expr]) -> Result<FloatValue<'ctx>, String> {
         if let Some(print_fn) = self.imported_functions.get("print") {
             let compiled_args: Vec<_> = args.iter()
                 .map(|arg| self.gen_expr(arg))
