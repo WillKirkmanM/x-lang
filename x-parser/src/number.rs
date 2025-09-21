e7ea1acdac134303e762753ff1cfdef572fa7d76
WillKirkmanM <WillKirkmanMoller@gmail.com>
2025-09-21 18:15:58 +0100

    Feat(number.rs): Use Float & Int Types to Parse Numbers

--- x-parser/src/number.rs
+++ x-parser/src/number.rs
@@ -1,17 +1,16 @@
+use crate::Rule;
 use pest::iterators::Pair;
 use x_ast::Expr;
-use crate::Rule;
 
 pub fn parse_number(pair: Pair<Rule>) -> Expr {
     let num_str = pair.as_str();
     if num_str.contains('.') {
-        Expr::Number(num_str.parse::<f64>().unwrap_or_else(|e| {
+        Expr::Float(num_str.parse::<f64>().unwrap_or_else(|e| {
             panic!("Failed to parse float '{}': {}", num_str, e);
         }))
     } else {
-        let int_val: i64 = num_str.parse().unwrap_or_else(|e| {
+        Expr::Int(num_str.parse::<i64>().unwrap_or_else(|e| {
             panic!("Failed to parse integer '{}': {}", num_str, e);
-        });
-        Expr::Number(int_val as f64)
+        }))
     }
 }
