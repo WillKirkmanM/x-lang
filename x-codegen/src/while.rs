ff8249dd59026e9a08ca4dd4fc2c7235d84c60e3
WillKirkmanM <WillKirkmanMoller@gmail.com>
2025-09-21 17:48:58 +0100

    Feat(while.rs): While Loop Generation

---
+++ x-codegen/src/while.rs
@@ -0,0 +1,53 @@
+use inkwell::FloatPredicate;
+use x_ast::{Expr, Statement};
+
+use crate::CodeGen;
+
+impl<'ctx> CodeGen<'ctx> {
+    pub fn gen_while_loop(
+        &mut self,
+        condition: &Expr,
+        body: &[Statement],
+    ) -> Result<(), String> {
+        let parent = self.builder.get_insert_block().unwrap();
+        let function = parent.get_parent().unwrap();
+        
+        let cond_bb = self.context.append_basic_block(function, "while.cond");
+        let body_bb = self.context.append_basic_block(function, "while.body");
+        let end_bb = self.context.append_basic_block(function, "while.end");
+        
+        self.builder.build_unconditional_branch(cond_bb)
+            .map_err(|e| e.to_string())?;
+        
+        self.builder.position_at_end(cond_bb);
+        let cond_val = self.gen_expr(condition)?;
+        
+        let zero = self.context.f64_type().const_float(0.0);
+        let comparison = self.builder
+            .build_float_compare(
+                FloatPredicate::ONE,
+                cond_val.into_float_value(),
+                zero,
+                "while.cmp"
+            )
+            .map_err(|e| e.to_string())?;
+        
+        self.builder
+            .build_conditional_branch(comparison, body_bb, end_bb)
+            .map_err(|e| e.to_string())?;
+        
+        self.builder.position_at_end(body_bb);
+        
+        for stmt in body {
+            self.gen_statement(stmt)?;
+        }
+        
+        self.builder
+            .build_unconditional_branch(cond_bb)
+            .map_err(|e| e.to_string())?;
+        
+        self.builder.position_at_end(end_bb);
+        
+        Ok(())
+    }
+}
