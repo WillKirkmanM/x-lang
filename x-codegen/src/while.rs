d58a82c85fb5abfc5eb9a18fc13b6246701325be
WillKirkmanM <WillKirkmanMoller@gmail.com>
2025-09-22 21:22:33 +0100

    Feat(while.rs): Include Self's Type When Generating IR for While Loops to Facilitate Heavier Invariant Optimisations

--- x-codegen/src/while.rs
+++ x-codegen/src/while.rs
@@ -1,5 +1,5 @@
 use inkwell::FloatPredicate;
-use x_ast::{Expr, Statement};
+use x_ast::{Expr, Statement, Type};
 
 use crate::CodeGen;
 
@@ -8,46 +8,49 @@ impl<'ctx> CodeGen<'ctx> {
         &mut self,
         condition: &Expr,
         body: &[Statement],
+        self_type: Option<&Type>,
     ) -> Result<(), String> {
         let parent = self.builder.get_insert_block().unwrap();
         let function = parent.get_parent().unwrap();
-        
+
         let cond_bb = self.context.append_basic_block(function, "while.cond");
         let body_bb = self.context.append_basic_block(function, "while.body");
         let end_bb = self.context.append_basic_block(function, "while.end");
-        
-        self.builder.build_unconditional_branch(cond_bb)
+
+        self.builder
+            .build_unconditional_branch(cond_bb)
             .map_err(|e| e.to_string())?;
-        
+
         self.builder.position_at_end(cond_bb);
-        let cond_val = self.gen_expr(condition)?;
-        
+        let cond_val = self.gen_expr(condition, self_type)?;
+
         let zero = self.context.f64_type().const_float(0.0);
-        let comparison = self.builder
+        let comparison = self
+            .builder
             .build_float_compare(
                 FloatPredicate::ONE,
                 cond_val.into_float_value(),
                 zero,
-                "while.cmp"
+                "while.cmp",
             )
             .map_err(|e| e.to_string())?;
-        
+
         self.builder
             .build_conditional_branch(comparison, body_bb, end_bb)
             .map_err(|e| e.to_string())?;
-        
+
         self.builder.position_at_end(body_bb);
-        
+
         for stmt in body {
-            self.gen_statement(stmt)?;
+            self.gen_statement(stmt, self_type)?;
         }
-        
+
         self.builder
             .build_unconditional_branch(cond_bb)
             .map_err(|e| e.to_string())?;
-        
+
         self.builder.position_at_end(end_bb);
-        
+
         Ok(())
     }
 }
